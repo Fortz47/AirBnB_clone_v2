981cef203c0bb905b6595e969879c85a20f45aa3
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-01-22 14:28:14 +0000

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,11 +2,23 @@
 """starts a Flask web application:"""
 
 from models import storage
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.teardown_appcontext()
-def teardown():
+
+@app.teardown_appcontext
+def teardown(exception):
     """closes sqlalchemy session"""
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """lists all states"""
+    states = storage.all('State').values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
